aa830cb195116b77ae0e1e6447e3131bbe2096c8
Hadrien-Robin <hadri.r@gmail.com>
2024-01-08 11:13:14 +0100

    Added the generate_frame that generate a frame sprite around a given sprite's image Added the CustomSprite class for inheritance of the new generate_frame function

--- src/components.py
+++ src/components.py
@@ -3,8 +3,21 @@ This file contains the classes for the components of the game
 """
 
 import pygame as pg
+import os
 
-class ImageSprite(pg.sprite.Sprite):
+class CustomSprite(pg.sprite.Sprite):
+    
+    def generate_frame(self):
+        assets_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'assets'))
+        frame = ImageSprite(self.game, os.path.join(assets_dir, 'images','UI', 'frame_map.png'))
+        size = (self.image.get_width()*370/338,self.image.get_height()*612/578)
+        frame.image = slice_sprite(frame.image, 32, 32, 26, 38, size[0],size[1])    
+        frame.rect = frame.image.get_rect()
+        frame.rect.center = self.rect.center
+        frame.rect.centery += size[1]*8/(612*2)
+        return frame
+
+class ImageSprite(CustomSprite):
     """
     Sprite class for loading and displaying images
     """
@@ -22,8 +35,10 @@ class ImageSprite(pg.sprite.Sprite):
         self.image.set_colorkey((0,0,0))
         # get the rect
         self.rect = self.image.get_rect()
+        
 
-class ShapeSprite(pg.sprite.Sprite):
+    
+class ShapeSprite(CustomSprite):
     """
     Sprite class for loading and displaying images
     """
@@ -42,7 +57,7 @@ class ShapeSprite(pg.sprite.Sprite):
 
         # get the rect
         self.rect = self.image.get_rect()        
-
+    
 class TextSprite(pg.sprite.Sprite):
     """
     Sprite class for displaying text
@@ -80,3 +95,82 @@ class TextSprite(pg.sprite.Sprite):
         Get the font object with the given size
         """
         return pg.font.Font(self.font_path, size)
+
+
+    
+
+
+#From Kokoko @stackoverflow
+
+def slice_sprite(sprite, left, right, top, bottom, width, height, draw_mode="SLICED"):
+    # get the size of the sprite
+    sprite_width = sprite.get_width()
+    sprite_height= sprite.get_height()
+
+    # create a new surface to draw the sliced sprite on
+    sliced_sprite = pg.Surface((width, height),pg.SRCALPHA)
+
+    # draw the top left side of the sprite
+    sliced_sprite.blit(sprite, (0, 0), (0, 0, left, top),special_flags = pg.BLEND_PREMULTIPLIED)
+
+    # draw the top right side of the sprite
+    sliced_sprite.blit(sprite, (width - right, 0),
+                       (sprite_width - right, 0, right, top),special_flags = pg.BLEND_PREMULTIPLIED)
+
+    # draw the bottom left side of the sprite
+    sliced_sprite.blit(sprite, (0, height - bottom),
+                       (0, sprite_height - bottom, left, bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+
+    # draw the bottom right side of the sprite
+    sliced_sprite.blit(sprite, (width - right, height - bottom),
+                       (sprite_width - right, sprite_height - bottom, right, bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+
+    match draw_mode:
+        case "SLICED":
+            # scale top and bottom sides of the sprite
+            sliced_sprite.blit(pg.transform.scale(sprite.subsurface(
+                left, 0, sprite_width - left - right, top), (width - left - right, top)), (left, 0),special_flags = pg.BLEND_PREMULTIPLIED)
+            sliced_sprite.blit(pg.transform.scale(sprite.subsurface(left, sprite_height - bottom,
+                               sprite_width - left - right, bottom), (width - left - right, bottom)), (left, height - bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+
+            # scale the center of the sprite
+            sliced_sprite.blit(pg.transform.scale(sprite.subsurface(left, top, sprite_width - left - right,
+                               sprite_height - top - bottom), (width - left - right, height - top - bottom)), (left, top),special_flags = pg.BLEND_PREMULTIPLIED)
+
+            # scale left and right sides of the sprite
+            sliced_sprite.blit(pg.transform.scale(sprite.subsurface(
+                0, top, left, sprite_height - top - bottom), (left, height - top - bottom)), (0, top),special_flags = pg.BLEND_PREMULTIPLIED)
+            sliced_sprite.blit(pg.transform.scale(sprite.subsurface(sprite_width - right, top, right,
+                               sprite_height - top - bottom), (right, height - top - bottom)), (width - right, top),special_flags = pg.BLEND_PREMULTIPLIED)
+
+        case "TILED":
+            # tile the center of the sprite
+            for x in range(left, width - right, sprite_width - left - right):
+                for y in range(top, height - bottom, sprite_height - top - bottom):
+                    sliced_sprite.blit(sprite.subsurface(
+                        left, top, sprite_width - left - right, sprite_height - top - bottom), (x, y),special_flags = pg.BLEND_PREMULTIPLIED)
+
+            # tile top and bottom sides of the sprite
+            for x in range(left, width - right, sprite_width - left - right):
+                sliced_sprite.blit(sprite.subsurface(
+                    left, 0, sprite_width - left - right, top), (x, 0),special_flags = pg.BLEND_PREMULTIPLIED)
+                sliced_sprite.blit(sprite.subsurface(
+                    left, sprite_height - bottom, sprite_width - left - right, bottom), (x, height - bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+
+            # tile left and right sides of the sprite
+            for y in range(top, height - bottom, sprite_height - top - bottom):
+                sliced_sprite.blit(sprite.subsurface(
+                    0, top, left, sprite_height - top - bottom), (0, y),special_flags = pg.BLEND_PREMULTIPLIED)
+                sliced_sprite.blit(sprite.subsurface(
+                    sprite_width - right, top, right, sprite_height - top - bottom), (width - right, y),special_flags = pg.BLEND_PREMULTIPLIED)
+
+            # draw the corners of the sprite
+            sliced_sprite.blit(sprite.subsurface(0, 0, left, top), (0, 0),special_flags = pg.BLEND_PREMULTIPLIED)
+            sliced_sprite.blit(sprite.subsurface(
+                sprite_width - right, 0, right, top), (width - right, 0),special_flags = pg.BLEND_PREMULTIPLIED)
+            sliced_sprite.blit(sprite.subsurface(
+                0, sprite_height - bottom, left, bottom), (0, height - bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+            sliced_sprite.blit(sprite.subsurface(
+                sprite_width - right, sprite_height - bottom, right, bottom), (width - right, height - bottom),special_flags = pg.BLEND_PREMULTIPLIED)
+
+    return sliced_sprite
