8fd52d8f9933d1321c88630181b7c4addf2b7cbb
Hadrien-Robin <hadri.r@gmail.com>
2024-04-27 10:47:27 +0200

    -Added pygame to import for time keeping -Added two attributes to Event class; reference and LastEvent. First on serves to identifies the current Event menu and the second keep track of the last time an event was unload. -Modified the behavior of load_event. It now use the reference attributes instead. -Made change to dialog of event 1.

--- src/event.py
+++ src/event.py
@@ -1,3 +1,5 @@
+import pygame as pg
+
 class Event:
     def __init__(self,game):
         self.reset_event()
@@ -6,11 +8,12 @@ class Event:
     def reset_event(self):
         self.description = ""
         self.number = 0
+        self.reference = ""
         self.choice = {}
+        self.LastEvent = pg.time.get_ticks()
         
     def event_exit(self):
         self.reset_event()
-        self.number = 0
         
     def consequence(self,amount,stuff):
         if stuff == "Fame":
@@ -19,27 +22,29 @@ class Event:
             pass
         
     def load_event(self):
-        self.reference = str(self.number)
-        match self.reference:
+        if self.reference == "":
+            self.reference = str(self.number)
+        match self.reference: 
             case 'event_exit':
                 self.event_exit()
                 
             case '1':
                 self.description = "Attention, Pilot. Sensor array indicates the detection of a faint signal amidst our trajectory through space. Initiating scan protocol to determine the nature and origin of the signal. Stand by for further analysis."
-                self.choice = {"continue":'1_1'}
+                self.choice = {"Continue":'1_1'}
             
             case '1_1':
-                self.description = "Pilot, analysis of the signal source reveals an unexpected discovery. The probe has been identified as Voyager 2, a relic from the Solar System. Its presence in this remote sector of space is perplexing, as it exceeds known parameters of its intended trajectory. Recommend proceeding with caution and preparing for further investigation."
+                self.description = "Pilot, analysis of the signal source reveals an unexpected discovery. The probe has been identified as Voyager 2, a relic from the Solar System. Its presence in this remote sector of space is perplexing, as it exceeds known parameters of its intended trajectory. Recommend proceeding with caution and preparing for further investigation. \n \n Scrap it for parts.\n \n Contact nearest science base."
                 self.choice = {"Scrap it":'1_1_1',"Share coordinates":'1_1_2'}
             
             case '1_1_1':
-                self.description = "Acknowledged, Pilot. Initiating salvage procedure for Voyager 2 probe. Preparing to deploy retrieval drones to safely gather electronic components for analysis and potential utilization. Salvage operation commencing."
+                self.description = "Acknowledged, Pilot. Initiating salvage procedure for Voyager 2 probe. Preparing to deploy retrieval drones to safely gather electronic components for analysis and potential utilization. Salvage operation commencing.\n \n[Gained 1 electronic part.]"
                 self.consequence(1,"Electronic parts")
                 self.choice = {"Leave":'event_exit'}
         
             case '1_1_2':
-                self.description = "Understood, Pilot. Transmitting coordinates of Voyager 2's location to the nearest science base for further study. Data transfer initiated. This significant discovery has the potential to garner widespread attention and acclaim for your contribution to scientific knowledge. Transmission complete."
+                self.description = "Understood, Pilot. Transmitting coordinates of Voyager 2's location to the nearest science base for further study. Data transfer initiated. This significant discovery has the potential to garner widespread attention and acclaim for your contribution to scientific knowledge. Transmission complete. \n \n[Gained 10 fames.]"
                 self.consequence(10, "Fame")
                 self.choice = {"Leave":'event_exit'}
-        
+            
+
 EventCount = 1  
